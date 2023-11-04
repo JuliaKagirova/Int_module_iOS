683500e0f1c8b46aed1e06cae267d7ba9e625a4c
JK <kagirova.y@inbox.ru>
2023-11-04 22:26:27 +0300

    upd

--- Navigation/Profile/PhotosViewController.swift
+++ Navigation/Profile/PhotosViewController.swift
@@ -9,6 +9,9 @@ import iOSIntPackage
 class PhotosViewController: UIViewController {
     
     let photoIdent = "photoCell"
+    private var imagePublisherFacade = ImagePublisherFacade()
+    private var collectionImages : [UIImage] = Photos.shared.examples
+    let facade = ImagePublisherFacade()
     
     // MARK: Visual objects
     
@@ -31,20 +34,12 @@ class PhotosViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.title = "Photo Gallery"
         self.view.addSubview(photosCollectionView)
         self.photosCollectionView.dataSource = self
         self.photosCollectionView.delegate = self
         setupConstraints()
-        
-        //NotificationCenter
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
-                NotificationCenter.default.addObserver(self!,
-                                                       selector: #selector(self!.notificationAction),
-                                                       name: .reloadPhoto,
-                                                       object: nil)
-            }
+        facade.subscribe(self)
     }
     
     private func setupConstraints() {
@@ -55,23 +50,23 @@ class PhotosViewController: UIViewController {
             photosCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
     }
-
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
+        tabBarController?.tabBar.isHidden = true
+        facade.addImagesWithTimer(time: 2, repeat: 40, userImages: collectionImages)
+        self.receive(images: collectionImages)
     }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.isHidden = true
-        //NotificationCenter отписка
-        NotificationCenter.default.removeObserver(self)
+        tabBarController?.tabBar.isHidden = false
+        facade.removeSubscription(for: self)
     }
     
     @objc private func notificationAction(_ notification: NSNotification) {
-        guard let object = notification.object as? String else { return }
+        guard notification.object is String else { return }
         print("NotificationCenter in VC ")
-        print("object = \(object)")
     }
 }
 
@@ -90,21 +85,20 @@ extension PhotosViewController: UICollectionViewDelegateFlowLayout {
 extension PhotosViewController: UICollectionViewDataSource { 
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return Photos.shared.examples.count
+         return collectionImages.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: photoIdent, for: indexPath) as? PhotosCollectionViewCell else { return UICollectionViewCell()}
-        cell.configCellCollection(photo: Photos.shared.examples[indexPath.item])
+        cell.configCellCollection(photo: collectionImages[indexPath.item])
         return cell
     }
 }
 
 extension PhotosViewController: ImageLibrarySubscriber {
     func receive(images: [UIImage]) {
-        ImagePublisherFacade().removeSubscription(for: ImageLibrarySubscriber.self as! ImageLibrarySubscriber)
-        var somePic = ImagePublisherFacade()
-        somePic.addImagesWithTimer(time: 3, repeat: 25, userImages: [UIImage.init()])
+        self.collectionImages = images
+        photosCollectionView.reloadData()
     }
 }
     // NotificationCenter
