407790fc5d7daf7bde4f5cc290465d5a4c291ca0
JK <kagirova.y@inbox.ru>
2024-03-28 15:52:34 +0300

    upd

--- Navigation/NetworkService/NetworkManager.swift
+++ Navigation/NetworkService/NetworkManager.swift
@@ -7,82 +7,79 @@
 
 import UIKit
 
-enum AppConfiguration {
-    case people
-    case planets
-    case films
+enum AppConfiguration: String, CaseIterable {
+    case people = "https://swapi.dev/api/people/"
+    case planets =  "https://swapi.dev/api/planets/"
+    case films = "https://swapi.dev/api/films/"
+    
+    var url: URL? {
+        URL(string: self.rawValue)
+    }
 }
 
 struct NetworkManager {
     
-    static func request(for configuration: AppConfiguration) {
-        
-        switch configuration {
-        case .people:
-            if let url = URL(string: "https://swapi.dev/api/people/") {
-                let request = URLRequest(url: url)
-                let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                    if let error {
-                        print(error.localizedDescription)
-                        return
-                    }
-                    guard let response = response as? HTTPURLResponse,
-                            Array(200...300).contains(response.statusCode)
-                    else {
-                        print("response.statusCode")
-                        return
-                    }
-                    guard let data else { return }
-                    print(String(bytes: data, encoding: .utf8)!)
+    static func request(_ url: URL) {
+        switch url {
+        case AppConfiguration.people.url:
+            let url = AppConfiguration.people.url
+            let request = URLRequest(url: url!)
+            NetworkManager.request(url!)
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print(error.localizedDescription)
+                    return
                 }
-                task.resume()
-            }
-        case .planets:
-            if let url = URL(string: "https://swapi.dev/api/planets/") {
-                let request = URLRequest(url: url)
-                let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                    if let error {
-                        print(error.localizedDescription)
-                        return
-                    }
-                    guard let response = response as? HTTPURLResponse,
-                            Array(200...300).contains(response.statusCode)
-                    else {
-                        print("response.statusCode")
-                        return
-                    }
-                    guard let data else { return }
-                    print(String(bytes: data, encoding: .utf8)!)
+                guard let response = response as? HTTPURLResponse,
+                      Array(200...300).contains(response.statusCode)
+                else {
+                    print("response.statusCode")
+                    return
                 }
-                task.resume()
+                guard let data else { return }
+                print(String(bytes: data, encoding: .utf8)!)
             }
-        case .films:
-            if let url = URL(string: "https://swapi.dev/api/films/") {
-                let request = URLRequest(url: url)
-                let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                    if let error {
-                        print(error.localizedDescription)
-                        return
-                    }
-                    guard let response = response as? HTTPURLResponse,
-                            Array(200...300).contains(response.statusCode)
-                    else {
-                        print("response.statusCode")
-                        return
-                    }
-                    guard let data else { return }
-                    print(String(bytes: data, encoding: .utf8)!)
+            task.resume()
+        case AppConfiguration.planets.url:
+            let url = AppConfiguration.planets.url
+            let request = URLRequest(url: url!)
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print(error.localizedDescription)
+                    return
                 }
-                task.resume()
+                guard let response = response as? HTTPURLResponse,
+                      Array(200...300).contains(response.statusCode)
+                else {
+                    print("response.statusCode")
+                    return
+                }
+                guard let data else { return }
+                print(String(bytes: data, encoding: .utf8)!)
             }
+            task.resume()
+            NetworkManager.request(url!)
+        case AppConfiguration.films.url:
+            let url = AppConfiguration.films.url
+            let request = URLRequest(url: url!)
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print(error.localizedDescription)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse,
+                      Array(200...300).contains(response.statusCode)
+                else {
+                    print("response.statusCode")
+                    return
+                }
+                guard let data else { return }
+                print(String(bytes: data, encoding: .utf8)!)
+            }
+            task.resume()
+            NetworkManager.request(url!)
+        default:
+            break
         }
     }
 }
-
-
-
-
-
-
-
-
