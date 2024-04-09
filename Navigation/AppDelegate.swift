33c9b4356c546d443a9da813cefbc2d999f086b3
JK <kagirova.y@inbox.ru>
2024-04-09 14:39:52 +0300

    bug fix

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        var appConfiguration: AppConfiguration
-        
+
+        NetworkManager.request(.films)
         
         // create tab bar with feed and profile items
         let loginVC = LoginViewController()
--- Navigation/NetworkService/NetworkManager.swift
+++ Navigation/NetworkService/NetworkManager.swift
@@ -9,77 +9,39 @@ import UIKit
 
 enum AppConfiguration: String, CaseIterable {
     case people = "https://swapi.dev/api/people/"
-    case planets =  "https://swapi.dev/api/planets/"
+    case planets = "https://swapi.dev/api/planets/"
     case films = "https://swapi.dev/api/films/"
-    
-    var url: URL? {
-        URL(string: self.rawValue)
-    }
 }
 
 struct NetworkManager {
-    
-    static func request(_ url: URL) {
+    static func request(_ url: AppConfiguration) {
+        var request: URLRequest?
+        
         switch url {
-        case AppConfiguration.people.url:
-            let url = AppConfiguration.people.url
-            let request = URLRequest(url: url!)
-            NetworkManager.request(url!)
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error {
-                    print(error.localizedDescription)
-                    return
-                }
-                guard let response = response as? HTTPURLResponse,
-                      Array(200...300).contains(response.statusCode)
-                else {
-                    print("response.statusCode")
-                    return
-                }
-                guard let data else { return }
-                print(String(bytes: data, encoding: .utf8)!)
-            }
-            task.resume()
-        case AppConfiguration.planets.url:
-            let url = AppConfiguration.planets.url
-            let request = URLRequest(url: url!)
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error {
-                    print(error.localizedDescription)
-                    return
-                }
-                guard let response = response as? HTTPURLResponse,
-                      Array(200...300).contains(response.statusCode)
-                else {
-                    print("response.statusCode")
-                    return
-                }
-                guard let data else { return }
-                print(String(bytes: data, encoding: .utf8)!)
-            }
-            task.resume()
-            NetworkManager.request(url!)
-        case AppConfiguration.films.url:
-            let url = AppConfiguration.films.url
-            let request = URLRequest(url: url!)
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error {
-                    print(error.localizedDescription)
-                    return
-                }
-                guard let response = response as? HTTPURLResponse,
-                      Array(200...300).contains(response.statusCode)
-                else {
-                    print("response.statusCode")
-                    return
-                }
-                guard let data else { return }
-                print(String(bytes: data, encoding: .utf8)!)
-            }
-            task.resume()
-            NetworkManager.request(url!)
-        default:
+        case .people:
             break
+        case .planets:
+            break
+        case .films:
+            let url = URL(string: url.rawValue)!
+            request = URLRequest(url: url)
         }
-    }
+        guard let request else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                print("DataTask error:" + error.localizedDescription)
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                  Array(200...300).contains(response.statusCode)
+            else { return }
+            guard let data else { return }
+            print(String(bytes: data, encoding: .utf8))
+            print(response.statusCode, response.allHeaderFields)
+
+        }
+        task.resume()
+     }
 }
