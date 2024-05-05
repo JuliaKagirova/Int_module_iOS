92b4fc1cf342cdb6257908e0a1eba07521741848
JK <kagirova.y@inbox.ru>
2024-05-05 12:56:16 +0300

    Home work JSON. Part 1. Without time period

--- Navigation/Library/Time.swift
+++ Navigation/Library/Time.swift
@@ -8,17 +8,24 @@
 import Foundation
 
 struct Time: Decodable {
-        let name: String?
-        let orbitalPeriod: String?
+    let name: String?
+    var orbitalPeriod: String
     
     enum CodingKeys: String ,CodingKey {
-        
         case name
         case orbitalPeriod = "orbitalPeriod"
     }
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decodeIfPresent(String.self, forKey: .name)
-        self.orbitalPeriod = try container.decode(String.self, forKey: .orbitalPeriod)
-    }  
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        orbitalPeriod = try container.decode(String.self, forKey: .orbitalPeriod)
+        
+//        if let key = try container.decodeIfPresent (Int.self, forKey: .orbitalPeriod) {
+//            self .orbitalPeriod = key
+//        } else if let key = try container.decodeIfPresent(String.self, forKey: .orbitalPeriod) {
+//            self .orbitalPeriod = Int(key) ?? 0
+//        } else {
+//            self.orbitalPeriod = 0
+//        }
+    }
 }
