a89874fbbcf314733ee93b14b1ed0415d5ecb898
JK <kagirova.y@inbox.ru>
2024-05-28 15:20:31 +0300

    home work file manager

---
+++ Bundle. Sandbox. FileManager/Bundle. Sandbox. FileManager/TextPicker.swift
@@ -0,0 +1,8 @@
+//
+//  TextPicker.swift
+//  Bundle. Sandbox. FileManager
+//
+//  Created by Юлия Кагирова on 28.05.2024.
+//
+
+import Foundation
---
+++ Bundle. Sandbox. FileManager/Bundle. Sandbox. FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bundle. Sandbox. FileManager
+//
+//  Created by Юлия Кагирова on 28.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
